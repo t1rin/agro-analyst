cddbb9d4ff85cc163a71a33ca02f449b8abe7f7b
t1rin <t1rin@mail.ru>
2024-07-20 20:13:00 +0400

    update UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 
 dpg.create_context()
 
@@ -13,18 +14,31 @@ dpg.set_viewport_max_width(MAX_WIDTH)
 dpg.set_viewport_min_height(MIN_HEIGHT)
 dpg.set_viewport_min_width(MIN_WIDTH)
 
-class agroAnalyst:
-    def __init__(self):
-        with dpg.window(tag=WINDOW_ID):
-            dpg.add_text("Hello, world")
+def begin() -> None:
+    with dpg.window(tag=WINDOW_ID):
+        with dpg.menu_bar():
+            ...
 
-application = agroAnalyst()
+
+        with dpg.table(header_row=False, hideable=True, resizable=True):
+            dpg.add_table_column()
+            dpg.add_table_column(width_fixed=True, 
+                                 init_width_or_weight=DEFAULT_PANEL_WIDTH)
+
+            with dpg.table_row():
+                with dpg.child_window():
+                    ...
+                with dpg.child_window():
+                    ...
+
 
 if CUSTOM_THEME:
     dpg.bind_theme(global_theme)
 if CUSTOM_FONT:
     ... # TODO
     # dpg.bind_font(font)
+
+begin()
 
 dpg.set_primary_window(WINDOW_ID, True)
 
--- theme_settings/theme_registry.py
+++ theme_settings/theme_registry.py
@@ -9,8 +9,11 @@ with dpg.theme() as global_theme:
         dpg.add_theme_style(dpg.mvStyleVar_ItemInnerSpacing, 8, 8, category=dpg.mvThemeCat_Core)
 
         dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (24, 24, 24), category=dpg.mvThemeCat_Core)
-        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (24, 24, 24), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (50, 50, 55), category=dpg.mvThemeCat_Core)
         dpg.add_theme_color(dpg.mvThemeCol_PopupBg, (31, 31, 31), category=dpg.mvThemeCat_Core)
         dpg.add_theme_color(dpg.mvThemeCol_ChildBg, (31, 31, 31), category=dpg.mvThemeCat_Core)
         dpg.add_theme_color(dpg.mvThemeCol_MenuBarBg, (27, 30, 32), category=dpg.mvThemeCat_Core)
         dpg.add_theme_color(dpg.mvThemeCol_Text, (225, 225, 225), category=dpg.mvThemeCat_Core)
+
+    with dpg.theme_component(dpg.mvTable):
+        dpg.add_theme_style(dpg.mvStyleVar_CellPadding, 2, 0, category=dpg.mvThemeCat_Core)
